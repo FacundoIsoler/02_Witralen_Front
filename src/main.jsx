050673e55983d793a68027f0f0a5adf8748dc3ce
Facundo Leonel Isoler Pagani <facundoisoler@outlook.com>
2025-04-05 10:48:57 -0300

    rutas vercel

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter, useLocation } from 'react-router-dom';
-import App from './App.jsx';
-import Navbar from './components/navbar/Navbar.jsx';
-import Footer from './components/footer/Footer.jsx';
+import { HashRouter } from 'react-router-dom';
+import AppWrapper from './AppWrapper.jsx'; // creamos un nuevo archivo para separar lógica del router
 import './index.css';
-
-function Root() {
-  const location = useLocation();
-  const hideNavbar = location.pathname === '/login' || location.pathname === '/register' || location.pathname === '/';
-
-  return (
-    <>
-      {!hideNavbar && <Navbar />}
-      <App />
-      <Footer />
-    </>
-  );
-}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HashRouter>
-      <Root />
+      <AppWrapper />
     </HashRouter>
   </React.StrictMode>
 );
